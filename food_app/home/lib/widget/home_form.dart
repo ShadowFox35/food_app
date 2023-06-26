041532ad2e66cf82c9376715d0f7b36bd714a830
ShadowFox35 <anastasia97chernova@gmail.com>
2023-06-26 19:04:26 +0200

    [PT-1] refactor: margins in home_form layout

--- food_app/home/lib/widget/home_form.dart
+++ food_app/home/lib/widget/home_form.dart
@@ -20,45 +20,43 @@ class HomeForm extends StatelessWidget {
                     height: 65,
                     child: Image.asset(ImagePaths.logoImage),
                   ),
-                  const SizedBox(width: 21),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Tamang',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      Text(
-                        'FoodService',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                    ],
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Tamang',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Text(
+                          'FoodService',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(height: 33),
-              SizedBox(
-                width: 213,
-                height: 243,
-                child: Image.asset(ImagePaths.homeImage),
-              ),
-              const SizedBox(
-                height: 41,
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 35, 0, 40),
+                child: SizedBox(
+                  width: 215,
+                  height: 245,
+                  child: Image.asset(ImagePaths.homeImage),
+                ),
               ),
               Text(
                 'Welcome',
                 style: Theme.of(context).textTheme.titleMedium,
               ),
-              const SizedBox(
-                height: 24,
-              ),
-              Text(
-                'It’s a pleasure to meet you. We are excited that you’re here so let’s get started!',
-                style: Theme.of(context).textTheme.bodyMedium,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 60,
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 25, 0, 60),
+                child: Text(
+                  'It’s a pleasure to meet you. We are excited that you’re here so let’s get started!',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.center,
+                ),
               ),
               const SizedBox(
                 width: 335,
