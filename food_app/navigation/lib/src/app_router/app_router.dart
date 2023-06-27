536e595dab25a331dfe9c158148a326684151645
ShadowFox35 <anastasia97chernova@gmail.com>
2023-06-27 22:07:57 +0200

    [PT-1] feat: auto router pathes

--- food_app/navigation/lib/src/app_router/app_router.dart
+++ food_app/navigation/lib/src/app_router/app_router.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 
-// part 'app_router.gr.dart';
+import 'package:home/home.dart';
+import 'package:home/screen/menu_screen.dart';
 
-// @MaterialAutoRouter(
-//   replaceInRouteName: 'Page,Route',
-//   routes: <AutoRoute>[
-//     AutoRoute(
-//       path: '/',
-//       page: HomeScreen,
-//       initial: true,
-//     ),
-//   ],
-// )
-// // extend the generated private router
-// class AppRouter extends _$AppRouter {}
+part 'app_router.gr.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: <AutoRoute>[
+    AutoRoute(
+      path: '/',
+      page: HomeScreen,
+      initial: true,
+    ),
+    AutoRoute(
+      name: 'menu',
+      page: MenuScreen,
+      // path: 'menu_screen/:id',
+    ),
+  ],
+)
+// extend the generated private router
+class AppRouter extends _$AppRouter {}
