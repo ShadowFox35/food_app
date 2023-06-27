84fcc6c3a2cccbdbb88e904bf3b2d9da40d0337f
ShadowFox35 <anastasia97chernova@gmail.com>
2023-06-27 21:28:43 +0200

    [PT-1] add: navigation package

---
+++ food_app/home/lib/widget/menu_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// import 'package:core_ui/core_ui.dart';
+
+class MenuForm extends StatelessWidget {
+  const MenuForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Menu'),
+      ),
+    );
+  }
+}
---
+++ food_app/navigation/lib/src/app_router/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+
+// part 'app_router.gr.dart';
+
+// @MaterialAutoRouter(
+//   replaceInRouteName: 'Page,Route',
+//   routes: <AutoRoute>[
+//     AutoRoute(
+//       path: '/',
+//       page: HomeScreen,
+//       initial: true,
+//     ),
+//   ],
+// )
+// // extend the generated private router
+// class AppRouter extends _$AppRouter {}
